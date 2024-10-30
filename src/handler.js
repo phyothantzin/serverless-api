027ed52545ec68b9427e46d8830cd75a014850e2
phyothantzin <phyothantzin.w@gmail.com>
2024-10-30 18:41:26 +0630

    configure neon serverless database and test

--- src/handler.js
+++ src/handler.js
@@ -1,13 +1,20 @@
 import serverless from "serverless-http";
 import express from "express";
-// const serverless = require("serverless-http");
-// const express = require("express");
+import { neon, neonConfig } from "@neondatabase/serverless";
+
 const app = express();
 
-app.get("/", (req, res, next) => {
+async function dbClient() {
+  const sql = neon(process.env.DATABASE_URL);
+  return sql;
+}
+
+app.get("/", async (req, res, next) => {
+  const sql = await dbClient();
+  const [results] = await sql`SELECT now();`;
   return res.status(200).json({
     message: "Hello from root!",
-    DATABASE_URL: process.env.DATABASE_URL,
+    results: results.now,
   });
 });
 
