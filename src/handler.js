bac37bb9503fbb0d309cfd4fc76bbe61451ccfd0
phyothantzin <phyothantzin.w@gmail.com>
2024-10-30 14:25:53 +0630

    init project & setup serverless offline

---
+++ src/handler.js
@@ -0,0 +1,29 @@
+import serverless from "serverless-http";
+import express from "express";
+// const serverless = require("serverless-http");
+// const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+app.get("/hello", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
+
+export const handler = serverless(app);
